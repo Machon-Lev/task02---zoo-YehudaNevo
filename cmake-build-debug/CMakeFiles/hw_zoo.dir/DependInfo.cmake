8b97f6ca8e1babdb5a7c92633dcdb2ccfc154f7e
Yehuda Nevo <45956551+YehudaNevo@users.noreply.github.com>
2023-05-10 19:23:39 +0300

    change the cmake file

---
+++ cmake-build-debug/CMakeFiles/hw_zoo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/Animal.cpp" "CMakeFiles/hw_zoo.dir/Animal.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/Animal.cpp.obj.d"
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/Goose.cpp" "CMakeFiles/hw_zoo.dir/Goose.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/Goose.cpp.obj.d"
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/Lion.cpp" "CMakeFiles/hw_zoo.dir/Lion.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/Lion.cpp.obj.d"
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/Monkey.cpp" "CMakeFiles/hw_zoo.dir/Monkey.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/Monkey.cpp.obj.d"
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/Zoo.cpp" "CMakeFiles/hw_zoo.dir/Zoo.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/Zoo.cpp.obj.d"
+  "C:/Users/User/CLionProjects/task02---zoo-YehudaNevo/main.cpp" "CMakeFiles/hw_zoo.dir/main.cpp.obj" "gcc" "CMakeFiles/hw_zoo.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/hw_zoo.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_zoo.dir/Animal.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/Animal.cpp.obj.d"
+  "CMakeFiles/hw_zoo.dir/Goose.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/Goose.cpp.obj.d"
+  "CMakeFiles/hw_zoo.dir/Lion.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/Lion.cpp.obj.d"
+  "CMakeFiles/hw_zoo.dir/Monkey.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/Monkey.cpp.obj.d"
+  "CMakeFiles/hw_zoo.dir/Zoo.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/Zoo.cpp.obj.d"
+  "CMakeFiles/hw_zoo.dir/main.cpp.obj"
+  "CMakeFiles/hw_zoo.dir/main.cpp.obj.d"
+  "hw_zoo.exe"
+  "hw_zoo.exe.manifest"
+  "hw_zoo.pdb"
+  "libhw_zoo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_zoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
